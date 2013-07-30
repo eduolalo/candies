e2950f78070469c082212c0867a72519021b9df9
Christian <rcchristiane@gmail.com>
2013-07-29 21:51:30 -0500

    29-jul-13

---
+++ src/candies/router.js
@@ -0,0 +1,20 @@
+(function(namespace) {
+
+  var views = namespace.views;
+
+  var Router = Backbone.Router.extend({
+    
+    routes: {
+      'candies': 'main'
+    },
+
+    main: function() {
+      new views.Main({
+        el: $('#container')
+      });
+    }
+  });
+
+  new Router();
+
+})(candies);
